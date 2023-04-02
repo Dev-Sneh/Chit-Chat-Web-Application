0b6478bb7c38d10cb4c29fd7901ba182611c735d
Sneh (Smilyboyy) <69115172+Dev-Sneh@users.noreply.github.com>
2023-04-02 15:11:52 +0530

    Update index.js

--- Chit-Chat Webapp/Server/index.js
+++ Chit-Chat Webapp/Server/index.js
@@ -20,22 +20,6 @@ app.use("/api/message", messageRoutes);
 //---------------------------Deployment------------------------------------------
 
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === 'production') {
-
-  app.use(express.static(path.join(__dirname1, '../client/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
-  });
-}
-else {
-  app.get("/", (req, res) => {
-    res.send("API is not running");
-  });
-}
-
 
 //---------------------------Deployment------------------------------------------
 
